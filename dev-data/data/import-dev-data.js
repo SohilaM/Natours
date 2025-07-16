dc9f764d973af7b8c07caba14e80435155843bbb
SohilaM <111076455+SohilaM@users.noreply.github.com>
2025-07-16 19:43:58 +0300

    a script to import the data form JSON file to the DB

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose.connect(DB).then(() => console.log('DB connection successful'));
+
+// Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// Import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data succesfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Delete all data from DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data succesfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+if (process.argv[2] === '--import') importData();
+else if (process.argv[2] === '--delete') deleteData();
+console.log(process.argv);
