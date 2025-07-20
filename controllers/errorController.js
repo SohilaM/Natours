de10442e03cfeff873b55b21b36c12043ff63490
SohilaM <111076455+SohilaM@users.noreply.github.com>
2025-07-20 03:55:22 +0300

    handling duplicate DB fields errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,13 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errorResponse.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value: ${value}, please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -43,6 +50,8 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.errorResponse.code === 11000)
+      error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
