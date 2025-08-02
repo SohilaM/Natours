b84d4779803f1be9d2d871c68640695e077fe6c8
SohilaM <111076455+SohilaM@users.noreply.github.com>
2025-08-02 20:24:37 +0300

    setting up pug
    creating base template

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,13 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // global middlewares
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // set security HTTP headers
 app.use(helmet());
 
@@ -52,9 +59,6 @@ app.use(
   }),
 );
 
-// serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // test middleware
 app.use((req, res, next) => {
   req.reqTime = new Date().toISOString();
@@ -62,6 +66,13 @@ app.use((req, res, next) => {
 });
 
 // routes
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Sohila',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
