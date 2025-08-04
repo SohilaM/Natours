b5911c2561ac2cc0da972cc68bc043c320968150
SohilaM <111076455+SohilaM@users.noreply.github.com>
2025-08-04 15:50:13 +0300

    fixing logging out error

--- public/js/login.js
+++ public/js/login.js
@@ -31,6 +31,7 @@ export const logout = async () => {
     });
     if (res.data.status === 'success') location.reload(true);
   } catch (error) {
+    console.log(error);
     showAlert('error', 'Error logging out! Try again.');
   }
 };
