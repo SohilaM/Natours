715bed452611691f0b722c80718eea5072b1cf8e
SohilaM <111076455+SohilaM@users.noreply.github.com>
2025-08-06 23:38:41 +0300

    welcome and resetPassword emails

--- utils/email.js
+++ utils/email.js
@@ -1,28 +1,73 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { convert } = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    // Activate in gmail "less secure app" option
-  });
+// new Email(user,url).sendWelcome();
 
-  // define the email options
-  const mailOptions = {
-    from: 'Sohila Mostafa',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`;
+    this.url = url;
+  }
 
-  // actually send the email
-  await transporter.sendMail(mailOptions);
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // brevo
+      return nodemailer.createTransport({
+        // service: 'brevo',
+        host: process.env.SENDINBLUE_HOST,
+        port: process.env.SENDINBLUE_PORT,
+        auth: {
+          user: process.env.SENDINBLUE_USERNAME,
+          pass: process.env.SENDINBLUE_PASSWORD,
+        },
+      });
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // send the actual email
+  async send(template, subject) {
+    // render the HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    // define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: convert(html, {
+        wordwrap: false,
+      }),
+    };
+
+    // create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (calid for only 10 minutes)',
+    );
+  }
 };
-
-module.exports = sendEmail;
